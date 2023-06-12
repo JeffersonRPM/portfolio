777b2219b548170387d4319e2b5893c864bb206b
Jefferson Martines <jefferson.martines@hotmail.com>
2023-06-12 01:18:15 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// setTimeout(function () {
+//     var targetAnchor = document.getElementById("section2");
+//     targetAnchor.scrollIntoView({ behavior: 'smooth' });
+// }, 5000);
+// setTimeout(function () {
+//     var targetAnchor = document.getElementById("section3");
+//     targetAnchor.scrollIntoView({ behavior: 'smooth' });
+// }, 10000);
+// setTimeout(function () {
+//     var targetAnchor = document.getElementById("section1");
+//     targetAnchor.scrollIntoView({ behavior: 'smooth' });
+// }, 15000);
+
+// window.onload = function () {
+//     window.scrollTo(0, 0);
+// };
