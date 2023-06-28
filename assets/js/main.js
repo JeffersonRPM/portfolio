e23e3c2df9ef82bf6120206e2d7b798cbab1d706
JeffersonRPM <jefferson.martines@hotmail.com>
2023-06-28 16:19:47 -0300

    feat: invert color black>white

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,45 @@
-setTimeout(function () {
-    var targetAnchor = document.getElementById("section2");
-    targetAnchor.scrollIntoView({ behavior: 'smooth' });
-}, 5000);
-setTimeout(function () {
-    var targetAnchor = document.getElementById("section3");
-    targetAnchor.scrollIntoView({ behavior: 'smooth' });
-}, 7500);
-setTimeout(function () {
-    var targetAnchor = document.getElementById("section4");
-    targetAnchor.scrollIntoView({ behavior: 'smooth' });
-}, 10000);
-setTimeout(function () {
-    var targetAnchor = document.getElementById("section5");
-    targetAnchor.scrollIntoView({ behavior: 'smooth' });
-}, 12500);
-setTimeout(function () {
-    var targetAnchor = document.getElementById("section1");
-    targetAnchor.scrollIntoView({ behavior: 'smooth' });
-}, 15000);
-
 window.onload = function () {
     window.scrollTo(0, 0);
 };
+
+if (window.innerWidth < 992) {
+    setTimeout(function () {
+        var targetAnchor = document.getElementById("section2");
+        targetAnchor.scrollIntoView({ behavior: 'smooth' });
+    }, 5000);
+
+    setTimeout(function () {
+        var targetAnchor = document.getElementById("section3");
+        targetAnchor.scrollIntoView({ behavior: 'smooth' });
+    }, 7500);
+
+    setTimeout(function () {
+        var targetAnchor = document.getElementById("section4");
+        targetAnchor.scrollIntoView({ behavior: 'smooth' });
+    }, 10000);
+
+    setTimeout(function () {
+        var targetAnchor = document.getElementById("section5");
+        targetAnchor.scrollIntoView({ behavior: 'smooth' });
+    }, 12500);
+
+    setTimeout(function () {
+        var targetAnchor = document.getElementById("section1");
+        targetAnchor.scrollIntoView({ behavior: 'smooth' });
+    }, 15000);
+} else {
+    setTimeout(function () {
+        var targetAnchor = document.getElementById("section2");
+        targetAnchor.scrollIntoView({ behavior: 'smooth' });
+    }, 5000);
+
+    setTimeout(function () {
+        var targetAnchor = document.getElementById("section4");
+        targetAnchor.scrollIntoView({ behavior: 'smooth' });
+    }, 10000);
+
+    setTimeout(function () {
+        var targetAnchor = document.getElementById("section1");
+        targetAnchor.scrollIntoView({ behavior: 'smooth' });
+    }, 15000);
+}
